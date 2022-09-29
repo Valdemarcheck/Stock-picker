04db6d7aa1ee7e63cde7a426360fddbeb42cb262
Valdemarcheck <valekzhaninvladimir58@gmail.com>
2022-09-29 18:57:33 +0500

    Remove unnecessary lines of code

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 
-def stock_picker(stocks) {
-    stocks_remaining = stocks
+def stock_picker(stocks)
     best_stock = [0, 0]
-}
+    stocks.each do |day|
+        stocks.each do |comparison_day|
+            break if day == comparison_day
+            if day - comparison_day > best_stock[1] - best_stock[0]
+                best_stock[0] = comparison_day
+                best_stock[1] = day
+            end
+        end
+    end
+    best_stock[0] = stocks.find_index(best_stock[0])
+    best_stock[1] = stocks.find_index(best_stock[1])
+    best_stock
+end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,15,8,6,1,10,20])
